7e0579f17e0b640b40fea7a8f0b5657597217c65
Dima Samodurov <dimasamodurov@gmail.com>
2015-02-17 19:30:39 +0200

    Turn chef-rvm for now as it cannot install gpg key.

--- roles/dev.rb
+++ roles/dev.rb
@@ -3,7 +3,7 @@ name 'dev'
 run_list(
     'recipe[java]',
     'recipe[imagemagick]',
-    'recipe[rvm::user]',
+    #'recipe[rvm::user]',
     'recipe[bootstrap::mongodb]',
     'recipe[bootstrap::productivity]'
 )
@@ -13,6 +13,7 @@ default_attributes(
                'jdk_version' => '7',
                'oracle' => {'accept_oracle_download_terms' => true}},
     'rvm' => {'installs' => {
+        'gpg_key' => '409B6B1796C275462A1703113804BB82D39DC0E3',
         'dev' => {
             'default_ruby' => 'ruby-2.2.0',
             'user_default_ruby' => 'ruby-2.2.0',
