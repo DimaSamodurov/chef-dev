8c49bf1e4cf1740f97c9c6b40c1ef06819e99fb0
Dima Samodurov <dmitriy.samodurov@sap.com>
2015-03-14 00:48:08 +0200

    Fix Rubocop offense.

--- site-cookbooks/bootstrap/attributes/mongodb.rb
+++ site-cookbooks/bootstrap/attributes/mongodb.rb
@@ -1,6 +1,10 @@
-
-default['mongodb']['config']['dbpath'] = '/data/mongodb' #- Location for mongodb data directory, defaults to "/var/lib/mongodb"
-default['mongodb']['config']['rest'] = true # - Enable the ReST interface of the webserver
-default['mongodb']['config']['smallfiles'] = true # - Modify MongoDB to use a smaller default data file size
-default['mongodb']['config']['oplogsize'] # - Specifies a maximum size in megabytes for the replication operation log
-default['mongodb']['config']['bind_ip'] = '127.0.0.1' # Configure from which address to accept connections
+#- Location for mongodb data directory, defaults to "/var/lib/mongodb"
+default['mongodb']['config']['dbpath'] = '/data/mongodb'
+# - Enable the ReST interface of the webserver
+default['mongodb']['config']['rest'] = true
+# - Modify MongoDB to use a smaller default data file size
+default['mongodb']['config']['smallfiles'] = true
+# - Specifies a maximum size in megabytes for the replication operation log
+default['mongodb']['config']['oplogsize']
+# Configure from which address to accept connections
+default['mongodb']['config']['bind_ip'] = '127.0.0.1'
